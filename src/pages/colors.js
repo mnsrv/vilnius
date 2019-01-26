9f92b499b2713a788caede3c37008da2f909fcf5
Aleksandr Mansurov <sasha.mansurov@gmail.com>
2019-01-26 20:03:34 +0300

    Update colors.js

--- src/pages/colors.js
+++ src/pages/colors.js
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 
 import Layout from '../components/Layout'
 
-const Color = ({ color }) => {
+const Color = ({ color, isLast }) => {
   return (
-    <div style={{ width: 50, height: 50, backgroundColor: color }} />
+    <div
+      style={{
+        width: '22%',
+        height: '3rem',
+        backgroundColor: color,
+        marginRight: isLast ? 0 : '4%',
+        marginBottom: '1rem'
+      }}
+    />
   )
 }
 
-export default () => (
-  <Layout title="Цвета">
-    <p>Собираю понравившиеся цвета</p>
-    <Color color="rgb(50,150,50)" />
-    <Color color="rgb(70,150,50)" />
-    <Color color="rgb(250,200,50)" />
-    <Color color="rgb(40,150,250)" />
-    <Color color="rgb(50,150,250)" />
-  </Layout>
-)
+export default class Colors extends PureComponent {
+  state = {
+    mode: 'light'
+  }
+
+  colors = [
+    '#FB090B', 'rgb(51,153,51)', 'rgb(255,204,51)', 'rgb(51,153,255)',
+    'rgb(255,204,204)', '#50e3c2', '#feebef', '#fffbe7'
+  ]
+
+  render() {
+    return (
+      <Layout title="Цвета">
+        <p>Собираю понравившиеся цвета</p>
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+          {this.colors.map((color, index) => (
+            <Color key={color} color={color} isLast={(index + 1) % 4 === 0} />
+          ))}
+        </div>
+      </Layout>
+    )
+  }
+}
