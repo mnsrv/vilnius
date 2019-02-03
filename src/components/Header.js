62d1cce0658c1c33b073171f55145f7a24323cb3
Aleksandr Mansurov <sasha.mansurov@gmail.com>
2019-02-04 02:54:26 +0300

    move header to separate file

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const ListLink = props => (
+  <li style={{ display: 'inline-block', marginRight: '1rem' }}>
+    <h4>
+      <Link to={props.to} className={props.className} activeClassName="active">
+        {props.children}
+      </Link>
+    </h4>
+  </li>
+)
+
+export default ({ index = false }) => (
+  <header>
+    <ul style={{ listStyle: 'none', marginLeft: 0 }}>
+      <ListLink to="/" className="home_link">
+        {index
+          ? <span key="white-dot" role="img" aria-label="white-dot">⚪️</span>
+          : <span key="black-dot" role="img" aria-label="black-dot">⚫️</span>}
+      </ListLink>
+      <ListLink to="/blog">Блог</ListLink>
+    </ul>
+  </header>
+)
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import { Link } from 'gatsby'
+
+import Header from './Header'
 import YearProgress from './YearProgress'
 
-const ListLink = props => (
-  <li style={{ display: 'inline-block', marginRight: '1rem' }}>
-    <h4>
-      <Link to={props.to} className={props.className} activeClassName="active">
-        {props.children}
-      </Link>
-    </h4>
-  </li>
-)
-
-export default ({ index = false, title, children }) => (
+export default ({ index, title, children }) => (
   <div className="container">
     <Helmet
       defaultTitle="mansurov.me"
@@ -21,16 +12,7 @@ export default ({ index = false, title, children }) => (
       htmlAttributes={{ lang: 'ru' }}
     />
     <YearProgress />
-    <header>
-      <ul style={{ listStyle: 'none', marginLeft: 0 }}>
-        <ListLink to="/" className="home_link">
-          {index
-            ? <span key="white-dot" role="img" aria-label="white-dot">⚪️</span>
-            : <span key="black-dot" role="img" aria-label="black-dot">⚫️</span>}
-        </ListLink>
-        <ListLink to="/blog">Блог</ListLink>
-      </ul>
-    </header>
+    <Header index={index} />
     <main>
       <h1>{title}</h1>
       {children}
