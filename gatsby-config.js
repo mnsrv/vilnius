81166da5290db9052f114b617547793c5df8d84a
Sasha Mansurov <sasha.mansurov@gmail.com>
2020-07-24 19:01:32 +0300

    Revert "No js"
    This reverts commit dbad96d7197916fb37f3510daf921e5c93ccc74a.

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,6 +79,5 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-styled-components',
-    'gatsby-plugin-no-javascript', // <-- Here is the plugin, make sure it is included last in the plugins array.
   ],
 }
